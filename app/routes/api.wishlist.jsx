36285f397e3a9f1c213f03c6972299a1c238562c
DanielZhang <thecndra@gmail.com>
2024-11-11 10:09:16 +0800

    Add wishlist action

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,19 +1,36 @@
 import { json } from "@remix-run/node";
-
+import db from "../db.server";
+import { cors } from "remix-utils";
 export async function loader(params) {
   return json({ ok: true, message: "Hello from the API" });
 }
 
 export async function action({ request }) {
   const { method } = request;
+  let data = await request.formData();
+  const { customerId, productId, shop } = { data };
+  if (!customerId || !productId || !shop) {
+    return json({
+      message: "Missing data. Required data: customerId, productId, shop",
+      method,
+    });
+  }
   switch (method) {
     case "POST":
-      return json({ message: "Success", method: "POST" });
-
-    case "PATCH":
-      return json({ message: "Success", method: "PATCH" });
+      const wishlist = await db.wishlist.create({
+        data: {
+          customerId,
+          productId,
+          shop,
+        },
+      });
+      const response = json({
+        message: "Product added to wishlist",
+        method: "POST",
+        wishlist,
+      });
 
     default:
-      return new Response("Method Not Allowed", { status: 405 });
+      break;
   }
 }
